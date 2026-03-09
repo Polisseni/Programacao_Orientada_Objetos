3cb7e96cc75ad7337a0aefde5d3af7767a9e5a46
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2026-03-09 20:14:06 -0300

    Texto

---
+++ TXT.py
@@ -0,0 +1,19 @@
+""""Objeto: Coisa material ou abstrata que é feito a partir de uma classe
+e pode ser descrito por meio dos seus atributos, métodos e estado atual
+
+
+Estado do Objeto: tamanho = 8.2 cm
+                  massa = baunilha
+                  peso = 54.3 g
+                  cobertura = pistache
+                  cozido = True
+                  temperatura = 55ºC
+
+
+Objetos abstratos: Uma consulta marcada no médico
+                   Um processo de venda
+                   Um compromisso ou reunião
+                   Uma aula na faculdade
+                   Uma transação bancária
+                   Uma reserva de voo
+                   Um erro do sistema"""
